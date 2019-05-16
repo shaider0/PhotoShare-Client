6febc5081517aaee51766d4b49813855ad59d463
SHaider0 <Samnoon.Haider@gmail.com>
2019-05-16 13:38:04 -0400

    remove extra "/" at the end of apiUrl

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://gentle-spire-82596.herokuapp.com/',
+  production: 'https://gentle-spire-82596.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
