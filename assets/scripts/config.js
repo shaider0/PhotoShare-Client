a74aa0ec8b527d21dc26865cc42003b656b5a229
SHaider0 <Samnoon.Haider@gmail.com>
2020-03-29 13:34:51 -0400

    remove extra /

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://floating-meadow-57055.herokuapp.com/',
+  production: 'https://floating-meadow-57055.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
